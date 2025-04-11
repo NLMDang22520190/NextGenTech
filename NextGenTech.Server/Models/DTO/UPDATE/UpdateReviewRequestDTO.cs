0971568f1721a69426d4ea7caa26ce4206220f07
22520187 <146148909+22520187@users.noreply.github.com>
2025-04-11 22:29:24 +0700

    Add ReviewController, Edit IReViewRepository, SQLReviewRepository

---
+++ NextGenTech.Server/Models/DTO/UPDATE/UpdateReviewRequestDTO.cs
@@ -0,0 +1,8 @@
+namespace NextGenTech.Server.Models.DTO.UPDATE
+{
+    public class UpdateReviewRequestDTO
+    {
+        public int Rating { get; set; }
+        public string? Comment { get; set; }
+    }
+}
--- NextGenTech.Server/Repositories/IReviewRepository.cs
+++ NextGenTech.Server/Repositories/IReviewRepository.cs
@@ -1,9 +1,13 @@
 using NextGenTech.Server.Models.Domain;
+using NextGenTech.Server.Models.DTO.ADD;
+using NextGenTech.Server.Models.DTO.UPDATE;
 
 namespace NextGenTech.Server.Repositories
 {
     public interface IReviewRepository : INextGenTechRepository<Review>
     {
-
+        Task<Review> AddReviewAsync(AddReviewRequestDTO request);
+        Task<Review?> UpdateReviewAsync(int reviewId, UpdateReviewRequestDTO request);
+        Task<bool> DeleteReviewAsync(int reviewId);
     }
 }
